53008ab9f8e98bef58c0a9a192c54a23cc10510d
Albert <101092983+asabatep@users.noreply.github.com>
2022-11-19 19:47:11 +0100

    hadolint: get rid of some lintian warnings

--- hadolint/build.sh
+++ hadolint/build.sh
@@ -5,12 +5,12 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get -y update
 apt-get -y upgrade
-apt-get -y install wget ghc haskell-stack dpkg-dev devscripts
+apt-get -y install wget ghc haskell-stack dpkg-dev devscripts help2man
 
 cd /work && rm -rf *
 wget -O hadolint_$VER.orig.tar.gz https://github.com/hadolint/hadolint/archive/refs/tags/v$VER.tar.gz
 tar xvf hadolint_$VER.orig.tar.gz
 cd hadolint-$VER
 cp -rv /contrib/debian .
-debuild -us -uc
+dpkg-buildpackage --no-sign
 mv /work/*deb /output
