b7cfc4e07e81782192596618468c3e838767f1d3
Albert <101092983+asabatep@users.noreply.github.com>
2023-10-19 18:39:46 +0200

    docker-{buildx,compose-v2}: build with go from trixie

--- docker-buildx/build.sh
+++ docker-buildx/build.sh
@@ -3,6 +3,26 @@ set -ex
 export DEBIAN_FRONTEND=noninteractive
 mkdir -p /work/$RELEASE
 
+if grep bookworm /etc/os-release ; then
+    cat > /etc/apt/preferences << EOF
+Package: *
+Pin: release a=stable
+Pin-Priority: 900
+
+Package: *
+Pin: release a=testing
+Pin-Priority: -1
+EOF
+    sed 's/bookworm/trixie/g' /etc/apt/sources.list.d/debian.sources > /etc/apt/sources.list.d/trixie.sources
+    apt-get -y update
+    apt-get -y upgrade
+    apt-get -t trixie -y install dh-golang golang-go
+else
+    apt-get -y update
+    apt-get -y upgrade
+    apt-get -y install dh-golang golang-go
+fi
+
 [ -z $VER ]    && VER=0.11.2
 [ -z $MOD ]    && MOD=-
 [ -z $BRANCH ] && BRANCH=0ubuntu1
@@ -10,11 +30,9 @@ mkdir -p /work/$RELEASE
 cd /work/$RELEASE
 rm -rf *
 
-apt-get -y update
-apt-get -y upgrade
 apt-get -y install dpkg-dev devscripts git dh-golang golang-go
 
-git clone -b ubuntu/$VER$MOD$BRANCH https://github.com/canonical/docker-buildx
+git clone --recurse-submodules -b ubuntu/$VER$MOD$BRANCH https://github.com/canonical/docker-buildx
 cd $(find . -maxdepth 1 -mindepth 1 -type d)
 
 curl -L https://github.com/canonical/docker-buildx/archive/refs/tags/upstream/$VER.tar.gz > ../docker-buildx_$VER.orig.tar.gz
--- docker-compose-v2/build.sh
+++ docker-compose-v2/build.sh
@@ -3,6 +3,26 @@ set -ex
 export DEBIAN_FRONTEND=noninteractive
 mkdir -p /work/$RELEASE
 
+if grep bookworm /etc/os-release ; then
+    cat > /etc/apt/preferences << EOF
+Package: *
+Pin: release a=stable
+Pin-Priority: 900
+
+Package: *
+Pin: release a=testing
+Pin-Priority: -1
+EOF
+    sed 's/bookworm/trixie/g' /etc/apt/sources.list.d/debian.sources > /etc/apt/sources.list.d/trixie.sources
+    apt-get -y update
+    apt-get -y upgrade
+    apt-get -t trixie -y install dh-golang golang-go
+else
+    apt-get -y update
+    apt-get -y upgrade
+    apt-get -y install dh-golang golang-go
+fi
+
 [ -z $VER ]    && VER=2.20.2
 [ -z $MOD ]    && MOD=+ds1
 [ -z $BRANCH ] && BRANCH=-0ubuntu1
@@ -10,9 +30,7 @@ mkdir -p /work/$RELEASE
 cd /work/$RELEASE
 rm -rf *
 
-apt-get -y update
-apt-get -y upgrade
-apt-get -y install dpkg-dev devscripts git dh-golang golang-go
+apt-get -y install dpkg-dev devscripts git 
 
 git clone --recurse-submodules -b ubuntu/$VER$MOD$BRANCH https://github.com/canonical/docker-compose-v2
 cd $(find . -maxdepth 1 -mindepth 1 -type d)
