bc4003c8a519f3f121fbe6660322a728821e3422
Albert <101092983+asabatep@users.noreply.github.com>
2023-12-10 12:32:47 +0100

    docker-{buildx,compose-v2}: better version handling

--- docker-buildx/build.sh
+++ docker-buildx/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ex
 export DEBIAN_FRONTEND=noninteractive
+export THISPKG=docker-buildx
 mkdir -p /work/$RELEASE
 
 if grep bookworm /etc/os-release ; then
@@ -23,21 +24,22 @@ else
     apt-get -y install dh-golang golang-go
 fi
 
-[ -z $VER ]    && VER=0.11.2
-[ -z $MOD ]    && MOD=-
-[ -z $BRANCH ] && BRANCH=0ubuntu1
-
 cd /work/$RELEASE
 rm -rf *
 
-apt-get -y install dpkg-dev devscripts git dh-golang golang-go
+FEED=/dev/shm/releases.atom
+apt-get -y install dpkg-dev devscripts git dh-golang golang-go xmlstarlet
+curl -o $FEED https://github.com/canonical/$THISPKG/releases.atom
 
-git clone --recurse-submodules -b ubuntu/$VER$MOD$BRANCH https://github.com/canonical/docker-buildx
+DVER=$(xmlstarlet sel -t -m "/*[local-name()='feed']/*[local-name()='entry']" -v "*[local-name()='id']" -n $FEED | grep ubuntu | awk -F/ '{print $NF}' )
+UVER=$(xmlstarlet sel -t -m "/*[local-name()='feed']/*[local-name()='entry']" -v "*[local-name()='id']" -n $FEED | grep upstream | awk -F/ '{print $NF}' )
+
+git clone --recurse-submodules -b ubuntu/$DVER https://github.com/canonical/$THISPKG
 cd $(find . -maxdepth 1 -mindepth 1 -type d)
 
-curl -L https://github.com/canonical/docker-buildx/archive/refs/tags/upstream/$VER.tar.gz > ../docker-buildx_$VER.orig.tar.gz
+curl -L https://github.com/canonical/$THISPKG/archive/refs/tags/upstream/$UVER.tar.gz -o ../${THISPKG}_${UVER}.orig.tar.gz
 
-NAME="repo.lol" EMAIL="pkgs@repo.lol" dch --nmu "Debian port of Ubuntu's docker-buildx"
+NAME="repo.lol" EMAIL="pkgs@repo.lol" dch --nmu "Debian port of Ubuntu's $THISPKG"
 sed -i s/UNRELEASED/$RELEASE/ ./debian/changelog
 
 dpkg-buildpackage --compression=gzip
