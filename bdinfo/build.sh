411f88414215aff2c9b0ead59fb32cc8bd2cf8c8
Albert <101092983+asabatep@users.noreply.github.com>
2024-05-08 14:30:13 +0200

    bdinfo: minor adj

--- bdinfo/build.sh
+++ bdinfo/build.sh
@@ -3,14 +3,17 @@ export LANG=C.UTF-8
 export DEBIAN_FRONTEND=noninteractive
 [ -z $VER ] && exit 1
 
+mkdir -p /work/$RELEASE
 apt-get -y update
 apt-get -y upgrade
 apt-get -y install libbluray-dev git build-essential dpkg-dev devscripts
 
-cd /work && rm -rf *
+cd /work/$RELEASE
 git clone https://github.com/schnusch/bdinfo
 cd bdinfo
 git reset --hard "$VER"
 cp -rv /contrib/debian .
+
 dpkg-buildpackage --no-sign
-mv /work/*deb /output
+mkdir -p /output/$RELEASE
+mv /work/$RELEASE/*deb /output/$RELEASE
